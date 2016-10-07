8546612d6c6a39582a986979410d463b054b1d31
Vaclav Novotny <vaclavnovotny@vaclavnovotny.eu>
2016-10-07 10:18:28 +0200

    Init commit

---
+++ scripts/example.js
@@ -0,0 +1,74 @@
+var CommentBox  = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                I am a Comment Form.
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    rawMarkup: function () {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="author">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
+var comments = [
+    {
+        id: 1,
+        author: "John Snow",
+        text: "Upon being guided into the inner-sanctum at the Oculus Connect developers conference."
+    },
+    {
+        id: 2,
+        author: "Disguised Toast",
+        text: "Snap is *reportedly* preparing for an IPO that could value it at $25B or higher"
+    }
+];
+
+ReactDOM.render(
+    <CommentBox data={comments} />,
+    document.getElementById('content')
+);
