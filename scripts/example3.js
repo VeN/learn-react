d2cc85d8d7c8d84f712173d8c3a22638e2286366
Vaclav Novotny <vaclavnovotny@vaclavnovotny.eu>
2016-10-14 11:41:12 +0200

    More examples

---
+++ scripts/example3.js
@@ -0,0 +1,20 @@
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <p>
+                Hello, <input type="text" placeholder="Your name here" />!
+                It is {this.props.date.toTimeString()}
+            </p>
+        );
+    }
+}
+
+function tick() {
+    ReactDOM.render(
+        <HelloWorld date={new Date()} />,
+        document.getElementById('app')
+    );
+}
+
+setInterval(tick, 500);
